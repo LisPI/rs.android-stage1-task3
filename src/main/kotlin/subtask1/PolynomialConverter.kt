149adac64b8fdca8ab672726b65fd088f0e324d3
LisPI <paschka.lis@gmail.com>
2020-05-02 12:16:32 +0300

    feat: add PolynomialConverter

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -2,10 +2,21 @@ package subtask1
 
 class PolynomialConverter {
 
-    // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
         if (numbers.isEmpty()) return null
 
-        return ""
+        var outputString = ""
+        for((index,coeff) in numbers.withIndex()){
+            if(coeff != 0){
+                outputString += coeff.toString() + "x^" + (numbers.size - index - 1) + " + "
+            }
+        }
+
+        outputString = outputString.replace("x^1", "x")
+        outputString = outputString.replace("x^0", "")
+        outputString = outputString.replace("1x", "x")
+        outputString = outputString.replace("+ -", "- ")
+
+        return outputString.substring(0,(outputString.length-3))
     }
 }
