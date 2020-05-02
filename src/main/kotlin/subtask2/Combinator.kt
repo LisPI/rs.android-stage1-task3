d25f30045a5be4cac311d15782f413c452bbaebb
LisPI <paschka.lis@gmail.com>
2020-05-02 12:48:22 +0300

    feat: add Combinator

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -2,8 +2,24 @@ package subtask2
 
 class Combinator {
 
-    // TODO: Complete the following function
+    private fun factorial(n : Int) : Int {
+        if((n == 0) or (n == 1)) return 1
+        var fact = 1
+        for(k in 1..n){
+            fact*=k
+        }
+        return fact
+    }
+
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val cnk = array[0]
+        val n = array[1]
+
+        for(k in 0..(n/2)){
+            if(cnk*factorial(k)*factorial(n-k) == factorial(n))
+                return k
+        }
+
+        return null
     }
 }
