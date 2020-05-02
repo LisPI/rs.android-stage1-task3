80fed18feb1aae1f00f6a87ead28c9f04f166090
LisPI <paschka.lis@gmail.com>
2020-05-02 19:21:05 +0300

    feat: add ArrayCalculator

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -2,8 +2,45 @@ package subtask3
 
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        val items = mutableListOf<Int>()
+
+        for(item in itemsFromArray)
+            if(item is Int)
+                items.add(item)
+
+        if(items.isEmpty())
+            return 0
+
+        if(numberOfItems >= items.size){
+            var product = 1
+            for(item in items)
+                product*=item
+            return product
+        }
+
+        items.sortDescending()
+
+        var product = 1
+
+        if(numberOfItems % 2 == 1){
+            product*=items.first()
+            items.removeAt(0)
+        }
+
+        var pairsProduct = mutableListOf<Int>()
+
+        for(itemIndex in items.indices){
+            if(itemIndex+1 != items.size)
+                if(items[itemIndex] * items[itemIndex+1] > 0)
+                    pairsProduct.add(items[itemIndex] * items[itemIndex+1])
+        }
+
+        pairsProduct.sortDescending()
+
+        for(i in 1..numberOfItems/2)
+            product*=pairsProduct[i-1]
+
+        return product
     }
 }
