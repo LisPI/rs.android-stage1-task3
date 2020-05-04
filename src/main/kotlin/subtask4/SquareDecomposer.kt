1509af041aa131a41ae604211316bbe3007b82b4
LisPI <paschka.lis@gmail.com>
2020-05-04 13:13:45 +0300

    feat: add SquareDecomposer

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -2,10 +2,26 @@ package subtask4
 
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
         if(number < 0) return null
 
-        return arrayOf(1)
+        var answerString = decomposeRecourse(number.toLong(),number*number.toLong(), "")
+        if(answerString == "")
+            return null
+
+        return answerString.split( " ", "end").filter { it != "" }.map { it.toInt() }.toTypedArray().reversed().toTypedArray()  //just for fun
+    }
+
+    private fun decomposeRecourse(n : Long, number : Long, str : String) : String{
+        for(k in n-1 downTo 1){
+            if(k*k < number){
+                val stringAnswer = decomposeRecourse(k, number - k * k, str+"$k ")
+                if(stringAnswer.contains("end"))
+                    return stringAnswer
+            }
+            if(number == k*k)
+                return "$str$k end"
+        }
+        return ""
     }
 }
